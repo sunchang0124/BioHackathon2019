5377969bca227057e68f81cf798f8387dc1db571
Johan van Soest <info@johanvansoest.nl>
2019-08-29 15:00:26 +0200

    Included building containers

--- demoDocker_1.sh
+++ demoDocker_1.sh
@@ -1,19 +1,25 @@
 ### curFolder=$(pwd)
 
+cd containers/createContainer/baseContainer
+docker build -t datasharing/basecontainer .
+
 ## Run Party_1_Container get its unique encrypt key, file UUID, VerifyKey
 cd containers/createContainer/Party_1_Container
+docker build -t datasharing/party_1 .
 docker run --rm --add-host dockerhost:192.168.65.2 \
 -v $(pwd)/input.json:/input.json \
 -v $(pwd)/encryption:/encryption datasharing/party_1
 
 ## Run Party_2_Container its unique encrypt key, file UUID, VerifyKey
 cd ../Party_2_Container
+docker build -t datasharing/party_2 .
 docker run --rm --add-host dockerhost:192.168.65.2 \
 -v $(pwd)/input.json:/input.json \
 -v $(pwd)/encryption:/encryption datasharing/party_2
 
 ## Run Party_2_Container its unique encrypt key, file UUID, VerifyKey
 cd ../Party_3_Container
+docker build -t datasharing/party_3 .
 docker run --rm --add-host dockerhost:192.168.65.2 \
 -v $(pwd)/input.json:/input.json \
 -v $(pwd)/encryption:/encryption datasharing/party_3
